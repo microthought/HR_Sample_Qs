8f1dfd0cd946b49e3c5aa5504ba7231575085666
Michael <michael.thelen@gmail.com>
2016-03-25 22:37:33 -0500

    Update ATM_Valid_PIN.js

--- CodeWars_Katas/ATM_Valid_PIN.js
+++ CodeWars_Katas/ATM_Valid_PIN.js
@@ -1,6 +1,7 @@
 /*
 ATM machines allow 4 or 6 digit PIN codes and PIN codes 
 cannot contain anything but exactly 4 digits or exactly 6 digits.
+hint: be warned about zeros up front, or non-number inputs
 */
 
 function validatePIN (pin) {
