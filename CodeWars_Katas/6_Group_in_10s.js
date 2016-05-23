a6214e3e26491d4a5664df354a89e2324b55bb73
Michael Thelen <michael.thelen@gmail.com>
2016-05-22 21:02:00 -0500

    Katas

---
+++ CodeWars_Katas/6_Group_in_10s.js
@@ -0,0 +1,44 @@
+/*
+
+Write a function groupIn10s which takes any number of arguments, and groups them into sets of 10s and sorts each group in ascending order.
+
+The return value should be an array of arrays, so that numbers between 0-9 inclusive are in position 0 and numbers 10-19 are in position 1, etc.
+
+Here's an example of the required output:
+
+const grouped = groupIn10s(8, 12, 38, 3, 17, 19, 25, 35, 50) 
+
+grouped[0]     // [3, 8]
+grouped[1]     // [12, 17, 19]
+grouped[2]     // [25]
+grouped[3]     // [35, 38]
+grouped[4]     // undefined
+grouped[5]     // [50]
+
+
+*/
+
+
+function groupIn10s() {
+var args = [...arguments].sort();
+var results = [];
+args.forEach(function(x) {
+		let pos = results[Math.floor(x/10)];
+		results[Math.floor(x/10)] = (pos) ? pos.concat(x) : [x]
+	})
+return results;
+}
+
+
+
+
+
+
+
+
+
+
+
+var derp = groupIn10s(8, 12, 38, 3, 17, 19, 25, 35, 50)
+
+console.log(derp[0])
